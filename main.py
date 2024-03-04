11612613d7641a01c6c0ad275d59ed7ca9e7b001
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-03 20:53:12 -0600

    Añadir clases de entidades, vistas, hilos, barreras, semáforos y eventos en main.py

--- main.py
+++ main.py
@@ -1,10 +1,62 @@
 import pygame
 import sys
+import threading
+import queue
+from pygame.locals import *
 
 # Constantes
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+FPS = 60
 
 # Entidades
+class Player:
+    pass
+
+class Asteroid:
+    pass
+
+class Enemy:
+    pass
 
 # Vistas
+class GameView:
+    pass
+
+# Hilos
+class PlayerThread(threading.Thread):
+    pass
+
+class EnemyThread(threading.Thread):
+    pass
+
+class AsteroidThread(threading.Thread):
+    pass
+
+class AudioThread(threading.Thread):
+    pass
+
+class GraphicsThread(threading.Thread):
+    pass
+
+class EnemyAI(threading.Thread):
+    pass
+
+# Barreras
+start_level_barrier = threading.Barrier(12)  # 10 foreground threads + 2 background threads
+start_music_barrier = threading.Barrier(2)  # Audio thread + main thread
+update_score_barrier = threading.Barrier(2)  # Game logic thread + graphics thread
+
+# Semáforos
+score_semaphore = threading.Semaphore(1)
+enemy_list_semaphore = threading.Semaphore(1)
+player_position_semaphore = threading.Semaphore(1)
+
+# Eventos
+new_level_event = threading.Event()
+game_over_event = threading.Event()
+shot_fired_event = threading.Event()
 
 # Main
+def main():
+    pass
