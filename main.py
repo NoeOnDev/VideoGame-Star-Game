77b0ee9dba9c07cfd2f79b494b54c048365e2370
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-04-06 01:49:54 -0600

    Corregir tamaño y posición de los botones de inicio y salida en main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 
 pygame.init()
+pygame.mixer.init()
 
 window_main_width = 800
 window_main_height = 470
@@ -14,8 +15,8 @@ start_button_color = (92, 169, 63)
 exit_button_color = (193, 49, 49)
 border_color = (23, 23, 23)
 
-button_width = 150
-button_height = 60
+button_width = 200
+button_height = 70
 border_width = 5
 
 start_button_x = window_main_width / 2 - button_width / 2
@@ -31,7 +32,7 @@ pygame.draw.rect(screen, start_button_color, pygame.Rect(start_button_x, start_b
 pygame.draw.rect(screen, border_color, pygame.Rect(exit_button_x - border_width, exit_button_y - border_width, button_width + 2 * border_width, button_height + 2 * border_width))
 pygame.draw.rect(screen, exit_button_color, pygame.Rect(exit_button_x, exit_button_y, button_width, button_height))
 
-font = pygame.font.Font(None, 36)
+font = pygame.font.Font(None, 40)
 
 start_text = font.render('Play', True, (0, 0, 0))
 exit_text = font.render('Exit', True, (0, 0, 0))
@@ -44,6 +45,9 @@ screen.blit(game_name_text, (game_name_x, game_name_y))
 
 screen.blit(start_text, (start_button_x + (button_width - start_text.get_width()) // 2, start_button_y + (button_height - start_text.get_height()) // 2))
 screen.blit(exit_text, (exit_button_x + (button_width - exit_text.get_width()) // 2, exit_button_y + (button_height - exit_text.get_height()) // 2))
+
+pygame.mixer.music.load('./src/sound/sound_main.mp3')
+pygame.mixer.music.play(-1)
 
 pygame.display.flip()
 
