536c0fc4973d661e3a24101e655dd0aa8678b079
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-04-06 01:05:31 -0600

    Corregir tamaño y posición de los botones de inicio y salida en main.py

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ import sys
 pygame.init()
 
 window_main_width = 800
-window_main_height = 600
+window_main_height = 501
 
 screen = pygame.display.set_mode((window_main_width, window_main_height))
+
+background = pygame.image.load('./src/img/background.png')
 
 start_button = pygame.image.load('./src/img/play.png')
 exit_button = pygame.image.load('./src/img/quit.png')
 
-start_button = pygame.transform.scale(start_button, (100, 50))  # Nuevo tamaño (100, 50)
-exit_button = pygame.transform.scale(exit_button, (100, 50))  # Nuevo tamaño (100, 50)
+start_button = pygame.transform.scale(start_button, (150, 75))
+exit_button = pygame.transform.scale(exit_button, (130, 50))
 
 start_button_x = window_main_width / 2 - start_button.get_width() / 2
 start_button_y = window_main_height / 2 - start_button.get_height() / 2
 exit_button_x = window_main_width / 2 - exit_button.get_width() / 2
 exit_button_y = start_button_y + start_button.get_height() + 30
+
+screen.blit(background, (0, 0))
 
 screen.blit(start_button, (start_button_x, start_button_y))
 screen.blit(exit_button, (exit_button_x, exit_button_y))
