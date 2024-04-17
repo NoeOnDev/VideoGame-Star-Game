638945f2f7da419108f098cf3951089a53a6f37a
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-04-17 14:47:30 -0600

    Actualizar manejo de clientes en server.py a través de asyncio

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-import threading
+import asyncio
 import json
 
 server_ip = '0.0.0.0'
@@ -8,16 +8,17 @@ server_port = 9009
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((server_ip, server_port))
 server.listen(2)
+server.setblocking(False)
 
 clientes = []
 
 estado_global = {}
 
-def manejar_cliente(cliente, id_jugador):
+async def manejar_cliente(cliente, id_jugador):
     global estado_global
     try:
         while True:
-            datos = cliente.recv(1024)
+            datos = await loop.sock_recv(cliente, 1024)
             if not datos:
                 break
 
@@ -28,7 +29,7 @@ def manejar_cliente(cliente, id_jugador):
                     estado_global[id_jugador] = movimiento
                     
                     for c in clientes:
-                        c.send((json.dumps(estado_global) + '\n').encode())
+                        await loop.sock_sendall(c, (json.dumps(estado_global) + '\n').encode())
     except ConnectionResetError:
         print("La conexión con el cliente ha sido cerrada inesperadamente.")
     finally:
@@ -38,18 +39,19 @@ def manejar_cliente(cliente, id_jugador):
             del estado_global[id_jugador]
             print(f"Player {id_jugador} ha sido eliminado")
 
-def aceptar_clientes():
+async def aceptar_clientes():
     id_jugador = 0
     while True:
-        cliente, addr = server.accept()
+        cliente, addr = await loop.sock_accept(server)
         print(f"Conexión desde {addr}")
         
         estado_global[id_jugador] = {'x': 400, 'y': 300}
         
         clientes.append(cliente)
         
-        threading.Thread(target=manejar_cliente, args=(cliente, id_jugador)).start()
+        loop.create_task(manejar_cliente(cliente, id_jugador))
         
         id_jugador += 1
 
-aceptar_clientes()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(aceptar_clientes())
